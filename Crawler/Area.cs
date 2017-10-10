abfa555713605ce3e801fcb3c2581da2928a913d
honggenxiang <766382576@qq.com>
2017-10-10 15:13:30 +0800

    调试完成

--- Crawler/Area.cs
+++ Crawler/Area.cs
@@ -4,26 +4,37 @@
 // ********************************************
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
+using Polly;
 
 namespace Crawler
 {
     public class Area
     {
+
+        readonly string[] specialName =
+            {"市辖区",
+               "省直辖县级行政区划",
+            "自治区直辖县级行政区划"
+        };
+
         private const string BaseUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016";
 
         private const string ProvinceUrl = "/index.html";
 
-        private const string CityUrl = "/32.html";
 
+        /// <summary>
+        /// 省
+        /// </summary>
+        /// <returns></returns>
         public List<AreaUnit> CollectProvince()
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            var doc = new HtmlWeb().LoadFromWebAsync(BaseUrl + ProvinceUrl, Encoding.GetEncoding("gb2312")).Result;
+            var doc = Policy.Handle<Exception>().Retry(5).Execute(() => new HtmlWeb().LoadFromWebAsync(BaseUrl + ProvinceUrl, Encoding.GetEncoding("gb2312")).Result);
             return doc.DocumentNode.SelectNodes("//tr/td/a").Select(c => new AreaUnit()
             {
                 Code = c.GetAttributeValue("href", string.Empty).TrimEnd(".html".ToArray()),
@@ -32,25 +43,130 @@ namespace Crawler
             }).ToList();
         }
 
-
+        //无县的市  东莞市(441900000000) 中山市(442000000000)
+        /// <summary>
+        /// 市
+        /// </summary>
+        /// <param name="provinces"></param>
+        /// <returns></returns>
         public List<AreaUnit> CollectCity(List<AreaUnit> provinces)
         {
-            var cities = new List<AreaUnit>();
-            foreach (var province in provinces)
+            var cities = new ConcurrentBag<AreaUnit>();
+            Parallel.ForEach(provinces, province =>
             {
-                var doc = new HtmlWeb().LoadFromWebAsync(BaseUrl + CityUrl, Encoding.GetEncoding("gb2312")).Result;
-                var result = doc.DocumentNode.SelectNodes("//table/table/tr").Select(
-                       c => new AreaUnit()
-                       {
-                           Code = c.ChildNodes[0].InnerText,
-                           Name = c.ChildNodes[1].InnerText,
-                           Level = Level.City
-                       }).ToList();
-                cities.AddRange(result);
-            }
-            return cities;
+                var doc = Policy.Handle<Exception>().Retry(5).Execute(() => new HtmlWeb().LoadFromWebAsync(BaseUrl + $"/{province.Code}.html", Encoding.GetEncoding("gb2312")).Result);
+                foreach (var node in doc.DocumentNode.SelectNodes("//tr[@class='citytr']"))
+                {
+                    var area = new AreaUnit()
+                    {
+                        Code = node.ChildNodes[0].FirstChild.InnerText,
+                        Name = node.ChildNodes[1].FirstChild.InnerText,
+                        Level = specialName.Contains(node.ChildNodes[1].FirstChild.InnerText) ? Level.None : Level.City
+                    };
+                    cities.Add(area);
+                    Console.WriteLine($"Level:{area.Level} Code:{area.Code}  Name:{area.Name}");
+                }
+            });
+            return cities.ToList();
+        }
+
+        //不存在区的市
+        private readonly string[] specialCities = { "东莞市", "中山市", "儋州市" };
+        /// <summary>
+        /// 区
+        /// </summary>
+        /// <param name="cities"></param>
+        /// <returns></returns>
+        public List<AreaUnit> CollectCounty(List<AreaUnit> cities)
+        {
+            var counties = new ConcurrentBag<AreaUnit>();
+            Parallel.ForEach(cities, city =>
+            {
+                if (specialCities.Contains(city.Name)) return;
+                var doc = Policy.Handle<Exception>().Retry(5).Execute(() => new HtmlWeb()
+                    .LoadFromWebAsync(BaseUrl + $"/{city.Code.Substring(0, 2)}/{city.Code.Substring(0, 4)}.html",
+                        Encoding.GetEncoding("gb2312")).Result);
+                foreach (var node in doc.DocumentNode.SelectNodes("//tr[@class='countytr']"))
+                {
+                    var area = new AreaUnit()
+                    {
+                        Code = node.ChildNodes[0].FirstChild.InnerText,
+                        Name = node.ChildNodes[1].FirstChild.InnerText,
+                        Level = specialName.Contains(node.ChildNodes[1].FirstChild.InnerText)
+                            ? Level.None
+                            : Level.County
+                    };
+                    counties.Add(area);
+                    Console.WriteLine($"Level:{area.Level} Code:{area.Code}  Name:{area.Name}");
+                }
+            });
+            return counties.ToList();
+        }
+
+
+        /// <summary>
+        /// 镇
+        /// </summary>
+        /// <param name="counties"></param>
+        /// <returns></returns>
+        public List<AreaUnit> CollectTown(List<AreaUnit> counties)
+        {
+            var towns = new ConcurrentBag<AreaUnit>();
+            Parallel.ForEach(counties, county =>
+            {
+                if (county.Level == Level.None) return;
+                var doc = Policy.Handle<Exception>().Retry(5).Execute(() => new HtmlWeb()
+                    .LoadFromWebAsync(
+                        BaseUrl +
+                        $"/{county.Code.Substring(0, 2)}/{county.Code.Substring(2, 2)}/{county.Code.Substring(0, 6)}.html",
+                        Encoding.GetEncoding("gb2312")).Result);
+                foreach (var node in doc.DocumentNode.SelectNodes("//tr[@class='towntr']"))
+                {
+                    var area = new AreaUnit()
+                    {
+                        Code = node.ChildNodes[0].FirstChild.InnerText,
+                        Name = node.ChildNodes[1].FirstChild.InnerText,
+                        Level = specialName.Contains(node.ChildNodes[1].FirstChild.InnerText) ? Level.None : Level.Town
+                    };
+                    towns.Add(area);
+                    Console.WriteLine($"Level:{area.Level} Code:{area.Code}  Name:{area.Name}");
+                }
+            });
+            return towns.ToList();
+        }
+
+
+        /// <summary>
+        /// 村
+        /// </summary>
+        /// <param name="towns"></param>
+        /// <returns></returns>
+        public List<AreaUnit> CollectVillage(List<AreaUnit> towns)
+        {
+            var villages = new ConcurrentBag<AreaUnit>();
+            Parallel.ForEach(towns, town =>
+            {
+                var doc = Policy.Handle<Exception>().Retry(5).Execute(() => new HtmlWeb()
+                    .LoadFromWebAsync(
+                        BaseUrl +
+                        $"/{town.Code.Substring(0, 2)}/{town.Code.Substring(2, 2)}/{town.Code.Substring(4, 2)}/{town.Code.Substring(0, 9)}.html",
+                        Encoding.GetEncoding("gb2312")).Result);
+                foreach (var node in doc.DocumentNode.SelectNodes("//tr[@class='villagetr']"))
+                {
+                    var area = new AreaUnit()
+                    {
+                        Code = node.ChildNodes[0].InnerText,
+                        Name = node.ChildNodes[2].InnerText,
+                        Level = Level.Village
+                    };
+                    villages.Add(area);
+                    Console.WriteLine($"Level:{area.Level} Code:{area.Code}  Name:{area.Name}");
+                }
+            });
+            return villages.ToList();
         }
     }
+
 
 
     public class AreaUnit
@@ -64,9 +180,10 @@ namespace Crawler
     public enum Level
     {
         /// <summary>
-        /// 未设置
+        /// 无
         /// </summary>
         None,
+
         /// <summary>
         /// 省
         /// </summary>
@@ -76,6 +193,21 @@ namespace Crawler
         /// 市
         /// </summary>
         City,
+
+        /// <summary>
+        /// 县
+        /// </summary>
+        County,
+
+        /// <summary>
+        /// 镇
+        /// </summary>
+        Town,
+
+        /// <summary>
+        /// 村
+        /// </summary>
+        Village
 
 
     }
--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Crawler
 {
     class Program
     {
+        //镇    东莞市(441900000000) 中山市(442000000000) 儋州市(460400000000)
         static void Main(string[] args)
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
             var provices = new Area().CollectProvince();
-            //foreach (var provice in provices)
-            //{
-            //    Console.WriteLine($" 编码:{provice.Code} 名称:{provice.Name}");
-            //}
+            File.AppendAllLines(Path.Combine(baseDir, "province.txt"), provices.Where(c => c.Level != Level.None).Select(c => c.Name));
 
             var cities = new Area().CollectCity(provices);
-            foreach (var city in cities)
+            File.AppendAllLines(Path.Combine(baseDir, "city.txt"), cities.Where(c => c.Level != Level.None).Select(c => c.Name));
+
+            var counties = new Area().CollectCounty(cities);
+            File.AppendAllLines(Path.Combine(baseDir, "county.txt"), counties.Where(c => c.Level != Level.None).Select(c => c.Name));
+
+            foreach (var county in counties)
             {
-                Console.WriteLine($" 编码:{city.Code} 名称:{city.Name}");
+                var towns = new Area().CollectTown(new List<AreaUnit>() { county });
+                File.AppendAllLines(Path.Combine(baseDir, "town.txt"), towns.Where(c => c.Level != Level.None).Select(c => c.Name));
+
+                foreach (var town in towns)
+                {
+                    var villages = new Area().CollectVillage(new List<AreaUnit>() { town });
+                    File.AppendAllLines(Path.Combine(baseDir, "village.txt"), villages.Where(c => c.Level != Level.None).Select(c => c.Name));
+                }
             }
 
             Console.WriteLine("处理完成");
